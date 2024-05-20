bd0522afb6eea468dc2a9270f1fcc7d14b3114b6
Real-Beginner <146841402+Real-Beginner@users.noreply.github.com>
2024-05-20 23:02:58 +0100

    Create function.h

---
+++ function.h
@@ -0,0 +1,9 @@
+double getMax(double array[]);
+double getAvg(double array[]);
+void runAllTests();
+void allZeroMax_testcase();
+void positiveMax_testcase();
+void negativeMax_testcase();
+void allZeroAvg_testcase();
+void positiveAvg_testcase();
+void negativeAvg_testcase();
